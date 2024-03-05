db4f77e26534e4aef285e2ee94b7d0a7a393fd52
Paras Chandra <paraschandra30@gmail.com>
2024-03-05 20:04:26 +0530

    init

---
+++ src/sections/index.js
@@ -0,0 +1,10 @@
+import Hero from './Hero'
+import Footer from './Footer'
+import Services from './Services'
+import CustomerReviews from './CustomerReviews'
+import PopularProducts from './PopularProducts'
+import SpecialOffer from './SpecialOffer'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+
+export { Hero, Footer, Services, CustomerReviews, PopularProducts, SpecialOffer, Subscribe, SuperQuality}
