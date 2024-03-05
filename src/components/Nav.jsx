f6999ff4b6008d5b11151b0b77e22b030a8408dc
Paras Chandra <paraschandra30@gmail.com>
2024-03-05 20:07:22 +0530

    added constants

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,6 +8,9 @@ const Nav = () => {
             <a href="/">
                 <img src={headerLogo} alt="logo"  width={130} height={29}/>
             </a>
+            <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+                
+            </ul>
         </nav>
     </header>
   )
