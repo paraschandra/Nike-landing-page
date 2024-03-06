a5b82dd1f668dea4cf8584d3f531bb404c9d687c
Paras Chandra <paraschandra30@gmail.com>
2024-03-06 12:49:17 +0530

    reviews section added

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import Nav from "./Nav";
 import PopularProductCard from "./PopularProductCard";
 import ShoeCard from "./ShoeCard";
 import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
 
 export {
     Button,
     Nav,
     PopularProductCard,
     ShoeCard,
-    ServiceCard
+    ServiceCard,
+    ReviewCard
 }
